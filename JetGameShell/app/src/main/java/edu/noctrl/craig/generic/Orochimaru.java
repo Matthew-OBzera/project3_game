f041397a8060f14e260f7ce832da191d222cc092
kjloveless <kjloveless@noctrl.edu>
2016-05-26 11:44:50 -0500

    updated sprite sheet and added class for second enemy

--- JetGameShell/app/src/main/java/edu/noctrl/craig/generic/Orochimaru.java
+++ JetGameShell/app/src/main/java/edu/noctrl/craig/generic/Orochimaru.java
@@ -2,15 +2,18 @@ package edu.noctrl.craig.generic;
 
 import android.graphics.Rect;
 
-public class Enemy2 extends Enemy {
+/**
+ * Created by Kyle on 5/26/2016.
+ */
+public class Orochimaru extends Enemy {
 
-    private final Rect rect = new Rect(12,300,100,379);
-    public Enemy2(Stage stage) {
+    private final Rect rect = new Rect(333, 270, 398, 386);
+    public Orochimaru(Stage stage) {
         super(stage);
-        this.speed = 200;
-        this.health = 20;
-        this.timeToAdd = .5;
-        this.pointWorth = 5;
+        this.speed = 500;
+        this.health = 40;
+        this.timeToAdd = 1;
+        this.pointWorth = 15;
         this.substance = Collision.SolidAI;
         if(stage.callsForEnemyMovement)
         {
@@ -29,7 +32,7 @@ public class Enemy2 extends Enemy {
 
     @Override
     public Point3F getScale() {
-        return new Point3F(.4F,.4F,1);
+        return new Point3F(.55F,.55F,1);
     }
 
     @Override
